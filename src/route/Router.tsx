ccb9a6cd6ef169f32c954dc4d6994d0286f165e2
csyun <csyun@creativelab-inc.com>
2022-11-17 16:48:38 +0900

    [Update] Router.tsx 폴더 변경

--- src/route/Router.tsx
+++ src/route/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import {
   MyRoom,
   NotFound,
